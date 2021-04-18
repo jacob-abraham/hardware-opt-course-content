9880b192db393470b8747100fb3f086d5f897c28
Jacob Abraham <jacob.r.abraham@gmail.com>
2021-04-18 14:52:31 -0700

    added hadd example

---
+++ qemu-code-contents/examples/hadd/hadd.asm
@@ -0,0 +1,126 @@
+
+
+
+global hardware_hadd
+global emulate_hadd_1
+global emulate_hadd_2
+
+%define iterations 10000
+
+section .text
+
+; cycle counts
+; https://uops.info/table.html
+; for skylake
+
+; rdi=struct
+hardware_hadd:
+
+    mov rax, [rdi] ; data
+    mov rdx, [rdi+8] ; result
+
+
+    %ifndef BENCHMARK
+    movaps xmm0, [rax] ; load
+    %else
+
+    mov rcx, iterations
+    movaps xmm2, [rax] ; load
+    
+    .looptop:
+    ; for speed this should be removed
+    ; because of loop dependencies it is needed for benchmark
+    movaps xmm0, xmm2
+    %endif
+
+    haddps xmm0, xmm0 ; 6, 2
+    haddps xmm0, xmm0 ; 6, 2
+                      ; 12
+    
+    %ifdef BENCHMARK
+    loopne hardware_hadd.looptop
+    %endif
+
+    ; store
+    movss [rdx], xmm0
+
+    xor eax, eax
+    ret
+
+
+; rdi=struct
+emulate_hadd_1:
+
+    mov rax, [rdi] ; data
+    mov rdx, [rdi+8] ; result
+    
+    %ifndef BENCHMARK
+    movaps xmm0, [rax] ; load
+    %else
+
+    mov rcx, iterations
+    movaps xmm2, [rax] ; load
+    
+    .looptop:
+    ; for speed this should be removed
+    ; because of loop dependencies it is needed for benchmark
+    movaps xmm0, xmm2
+    %endif
+
+    ; A  B  C  D
+    ; C  D  A  B
+    ; AC BD CA DB
+    ; DB CA BD AC
+
+    movaps xmm1, xmm0            ; 0/1, 0.25
+    shufps xmm1, xmm0, 01001110b ; 1  , 1
+    addps xmm0, xmm1             ; 4  , 0.5
+    movaps xmm1, xmm0            ; 0/1, 0.25
+    shufps xmm1, xmm0, 00011011b ; 1  , 1
+    addss xmm0, xmm1             ; 4  , 0.5
+                                 ; 10/12
+
+    %ifdef BENCHMARK
+    loopne emulate_hadd_1.looptop
+    %endif
+    ; store
+    movss [rdx], xmm0
+
+    xor eax, eax
+    ret
+
+emulate_hadd_2:
+
+    mov rax, [rdi] ; data
+    mov rdx, [rdi+8] ; result
+
+    %ifndef BENCHMARK
+    movaps xmm0, [rax] ; load
+    %else
+
+    mov rcx, iterations
+    movaps xmm2, [rax] ; load
+    
+    .looptop:
+    ; for speed this should be removed
+    ; because of loop dependencies it is needed for benchmark
+    movaps xmm0, xmm2
+    %endif
+
+    movshdup xmm1,xmm0 ; 1, 1
+    addps xmm0, xmm1   ; 4, 0.5
+    movhlps xmm1, xmm0 ; 1, 1
+    addss xmm0, xmm1   ; 4, 0.5
+                       ; 12
+
+    %ifdef BENCHMARK
+    loopne emulate_hadd_2.looptop
+    %endif
+
+    ; store
+    movss [rdx], xmm0
+
+    xor eax, eax
+    ret
+
+%undef iterations
---
+++ qemu-code-contents/examples/hadd/test.c
@@ -0,0 +1,57 @@
+
+#include <immintrin.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "timing.h"
+
+struct test_args {
+    float *a;
+    float *result;
+};
+
+void *hardware_hadd(void *args);
+void *emulate_hadd_1(void *args);
+void *emulate_hadd_2(void *args);
+
+int main(__attribute((unused)) int argc, __attribute((unused)) char **argv) {
+
+    // cannot be freed
+    float *a = (float *)aligned_alloc(64, 4 * sizeof(float));
+    float *result = (float *)aligned_alloc(64, sizeof(float));
+
+    struct test_args arg = {.a = a, .result = result};
+
+    a[0] = 17.0f;
+    a[1] = 39.0f;
+    a[2] = 13.0f;
+    a[3] = 43.0f;
+
+    #ifdef BENCHMARK
+
+    printf("HARDWARE HADD  : ");
+    benchmark(hardware_hadd, (void *)(&arg), 50, 1000, 0b01, NULL);
+    printf("EMULATED HADD 1: ");
+    benchmark(emulate_hadd_1, (void *)(&arg), 50, 1000, 0b01, NULL);
+    printf("EMULATED HADD 2: ");
+    benchmark(emulate_hadd_2, (void *)(&arg), 50, 1000, 0b01, NULL);
+
+    #else
+
+    // accuracy check
+    hardware_hadd((void *)(&arg));
+    printf("HA : %5.2f+%5.2f+%5.2f+%5.2f=%5.2f\n", a[0], a[1], a[2], a[3],
+           *result);
+    emulate_hadd_1((void *)(&arg));
+    printf("EM1: %5.2f+%5.2f+%5.2f+%5.2f=%5.2f\n", a[0], a[1], a[2], a[3],
+           *result);
+    emulate_hadd_2((void *)(&arg));
+    printf("EM2: %5.2f+%5.2f+%5.2f+%5.2f=%5.2f\n", a[0], a[1], a[2], a[3],
+           *result);
+    
+    #endif
+}
