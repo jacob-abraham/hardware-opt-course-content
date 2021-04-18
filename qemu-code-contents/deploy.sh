27ad1bd32a9a0518288d001f6bdb6c3900e6d1ae
Jacob Abraham <jacob.r.abraham@gmail.com>
2021-04-18 14:52:20 -0700

    updated scripts

--- qemu-code-contents/deploy.sh
+++ qemu-code-contents/deploy.sh
@@ -3,6 +3,6 @@
 # clean before deploy
 for f in $(find . -name Makefile); do make -C $(dirname $f) clean; done;
 
-FILES="examples timing .vimrc README.txt"
+FILES="examples timing .vimrc README.txt .gdbinit .bash_aliases"
 
 tar czf - $FILES | ssh -p 5555 steve@localhost "rm -rf $FILES; tar xzf -"
